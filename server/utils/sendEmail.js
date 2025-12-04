d87ea3d0a9c49343ad3dba08513f74bd64bfd9b1
DuckNoLife <Taolaita789@gmail.com>
2025-12-05 01:13:14 +0700

    brevo add

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,36 +1,31 @@
 const nodemailer = require('nodemailer');
-
+// Transporter creation
 const sendEmail = async (options) => {
-    // Transporter creation
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
         // Secure connection check
-        secure: process.env.SMTP_PORT == 465, 
+        secure: process.env.SMTP_PORT == 465,
         auth: {
             user: process.env.SMTP_EMAIL,
             pass: process.env.SMTP_PASSWORD
         }
     });
-
     // Email message configuration
     const message = {
-        from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`, 
+        from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
         to: options.email,
         subject: options.subject,
-        text: options.message, 
+        text: options.message,
     };
-
     // Send email
     try {
         const info = await transporter.sendMail(message);
         console.log('Message sent: %s', info.messageId);
     } catch (err) {
         console.error('Error sending email:', err.message);
-        // Re-throw error for controller handling
-        throw err; 
+        throw err;
     }
 };
-
 // Module export
 module.exports = sendEmail;
